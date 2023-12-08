26b6ae4cffbb5b13043824c7cc2b285a9833a4dc
shubhammane21 <shubhammane21@hotmail.com>
2023-12-09 01:00:49 +0530

    Upload source code
    Portfolio upload1

---
+++ src/compotents/navbar/navbar.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import "./navbar.css";
+import * as FaIcons from "react-icons/fa";
+import { Link, animateScroll } from "react-scroll";
+
+import { navigationData } from "../../data";
+
+function Navbar() {
+  const [mobile, setMobile] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth < 900) {
+      setMobile(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 900) {
+        !mobile && setMobile(true);
+      } else if (mobile) {
+        setMobile(false);
+        setSidebar(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  return (
+    <>
+      <nav className="navbar">
+        <span
+          className="home-title-logo"
+          onClick={() => {
+            animateScroll.scrollToTop();
+            setSidebar(false);
+          }}
+        >
+          PORTFOLIO <FaIcons.FaLaptopCode />
+        </span>
+        {mobile && (
+          <div className="sidebar-toggle">
+            {sidebar ? (
+              <FaIcons.FaTimes
+                className="sidebar-toggle-logo"
+                onClick={() => setSidebar(!sidebar)}
+              />
+            ) : (
+              <FaIcons.FaBars
+                className="sidebar-toggle-logo"
+                onClick={() => setSidebar(!sidebar)}
+              />
+            )}
+          </div>
+        )}
+        {!mobile && (
+          <ul className={mobile ? "nav-items mobile-nav" : "nav-items"}>
+            {navigationData.map((navItem) => {
+              return (
+                <li className="nav-item" key={navItem.id}>
+                  <Link
+                    to={navItem.path}
+                    smooth={true}
+                    duration={1000}
+                    offset={navItem.offSet}
+                  >
+                    {navItem.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </nav>
+      <div className={sidebar ? "sidebar active" : "sidebar"}>
+        <ul className="sidebar-items">
+          {navigationData.map((navItem) => {
+            return (
+              <li className="sidebar-item" key={navItem.id}>
+                <Link
+                  to={navItem.path}
+                  smooth={true}
+                  duration={1000}
+                  offset={navItem.offSet}
+                  onClick={() => setSidebar(false)}
+                >
+                  {navItem.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,211 @@
+import * as FaIcons from "react-icons/fa";
+import * as SiIcons from "react-icons/si";
+import * as AiIcons from "react-icons/ai";
+import * as DiIcons from "react-icons/di";
+
+export const navigationData = [
+  {
+    id: 1,
+    name: "Home",
+    path: "scroll-to-home",
+    offSet: -53,
+  },
+  {
+    id: 2,
+    name: "Tech Stack",
+    path: "scroll-to-techstack",
+    offSet: -53,
+  },
+  {
+    id: 3,
+    name: "Projects",
+    path: "scroll-to-projects",
+    offSet: -155,
+  },
+  // {
+  //   id: 4,
+  //   name: "Recommendations",
+  //   path: "scroll-to-recommendations",
+  //   offSet: 0,
+  // },
+  {
+    id: 5,
+    name: "Contact",
+    path: "scroll-to-contact",
+    offSet: -53,
+  },
+];
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Portfolio",
+    description:
+      "Responsive portfolio design with ReactJS. Used react hooks, props, react-router, react-icons, react-scroll and CSS.",
+    sourceCodeUrl: "https://github.com/",
+  },
+  {
+    id: 2,
+    name: "Pacman",
+    description:
+      "Complete clone of Pacman game with implementaion of artificial intelligence for ghosts to chase down pacman.",
+    sourceCodeUrl: "https://github.com/",
+  },
+  {
+    id: 3,
+    name: "Navigation",
+    description:
+      "Responsive navbar and sidebar navigation with animations. Used react hooks, props, react-router, react-icons and CSS",
+    sourceCodeUrl: "https://github.com/",
+  },
+  {
+    id: 4,
+    name: "A* Search Algorithm",
+    description:
+      "Visualization of A* search algorithm. Using p5 a JavaScript library for visual graphics",
+    sourceCodeUrl: "https://github.com",
+  },
+  {
+    id: 5,
+    name: "Prize Bond Searcher",
+    description:
+      "A prize bond searcher. Used Java File.io to look for prize bond numbers present in one file and compare it with other. File can be fed with garbage data, as long as there are actual bond number in file it will work!",
+    sourceCodeUrl: "https://github.com/",
+  },
+  {
+    id: 6,
+    name: "2D Flappy Bird",
+    description:
+      "I made a flappy bird game using Pygame library in Python. Learned to implement the game mechanics i.e. vertical movement of bird, jump of bird, movement of pipes etc",
+    sourceCodeUrl: "https://github.com/",
+  },
+];
+
+export const socialMediaLinks = [
+  {
+    id: 1,
+    url: "https://www.youtube.com",
+    cName: "icon youtube-icon",
+    logo: <FaIcons.FaYoutube />,
+  },
+  {
+    id: 2,
+    url: "https://github.com",
+    cName: "icon github-icon",
+    logo: <FaIcons.FaGithub />,
+  },
+  {
+    id: 3,
+    url: "https://www.linkedin.com/",
+    cName: "icon linkedin-icon",
+    logo: <FaIcons.FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: "https://www.instagram.com/",
+    cName: "icon instagram-icon",
+    logo: <FaIcons.FaInstagram />,
+  },
+];
+
+export const techStack = [
+  {
+    id: 1,
+    stackName: "Languages",
+    logo: <FaIcons.FaCode />,
+    cName: "stack",
+    technologies: [
+      {
+        id: 2,
+        name: "JavaScript",
+        cName: "technology",
+        logo: <SiIcons.SiJavascript />,
+      },
+      {
+        id: 1,
+        name: "Java",
+        cName: "technology",
+        logo: <SiIcons.SiJavascript />,
+      },
+      {
+        id: 3,
+        name: "Solidity",
+        cName: "technology",
+        logo: <SiIcons.SiSolidity />,
+      },
+    ],
+  },
+  {
+    id: 2,
+    stackName: "Frameworks",
+    logo: <AiIcons.AiFillCodeSandboxCircle />,
+    cName: "stack",
+    technologies: [
+      {
+        id: 1,
+        name: "ReactJS",
+        cName: "technology",
+        logo: <SiIcons.SiReact />,
+      },
+      {
+        id: 2,
+        name: "NodeJS",
+        cName: "technology",
+        logo: <FaIcons.FaNodeJs />,
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        cName: "technology",
+        logo: <SiIcons.SiTypescript />,
+      },
+      {
+        id: 4,
+        name: "Bootstrap",
+        cName: "technology",
+        logo: <SiIcons.SiBootstrap />,
+      },
+      
+    ],
+  },
+  {
+    id: 3,
+    stackName: "Databases",
+    logo: <FaIcons.FaDatabase />,
+    cName: "stack",
+    technologies: [
+      {
+        id: 1,
+        name: "MongoDB",
+        cName: "technology",
+        logo: <SiIcons.SiMongodb />,
+      },
+      {
+        id: 2,
+        name: "SQL",
+        cName: "technology",
+        logo: <AiIcons.AiOutlineConsoleSql />,
+      },
+    ],
+  },
+  {
+    id: 4,
+    stackName: "Tools",
+    logo: <FaIcons.FaToolbox />,
+    cName: "stack",
+    technologies: [
+      {
+        id: 1,
+        name: "Git",
+        cName: "technology",
+        logo: <DiIcons.DiGitBranch />,
+      },
+      {
+        id: 2,
+        name: "GitHub",
+        cName: "technology",
+        logo: <FaIcons.FaGithub />,
+      },
+    ],
+  },
+];
