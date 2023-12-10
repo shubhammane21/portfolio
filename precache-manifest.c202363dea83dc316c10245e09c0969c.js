44aa360e8f64b125dc8ab9758e666cf86be73c44
shubhammane21 <shubhammane21@hotmail.com>
2023-12-11 03:42:14 +0530

    Updates

---
+++ precache-manifest.c202363dea83dc316c10245e09c0969c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2ab4c9bd49c8963a7443646ebbac88",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c4ca569414f3d62a72a3",
+    "url": "/portfolio/static/css/main.deecc697.chunk.css"
+  },
+  {
+    "revision": "de537629e49bffdd61d6",
+    "url": "/portfolio/static/js/2.daf359b4.chunk.js"
+  },
+  {
+    "revision": "ac090904937ff15ba3f7",
+    "url": "/portfolio/static/js/3.18a8d937.chunk.js"
+  },
+  {
+    "revision": "c4ca569414f3d62a72a3",
+    "url": "/portfolio/static/js/main.af131ffe.chunk.js"
+  },
+  {
+    "revision": "6fd9d4ba7f56fc24c283",
+    "url": "/portfolio/static/js/runtime~main.2554c565.js"
+  },
+  {
+    "revision": "ff8183167ca2db24e29ceaea211a7684",
+    "url": "/portfolio/static/media/myImg.ff818316.png"
+  }
+]);
